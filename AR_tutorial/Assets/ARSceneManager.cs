bddf2da72e30b44a30e33492aa86bffac198ae22
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2022-09-22 20:17:11 +0900

    make scene mission

--- AR_tutorial/Assets/ARSceneManager.cs
+++ AR_tutorial/Assets/ARSceneManager.cs
@@ -12,6 +12,17 @@ public class ARSceneManager : MonoBehaviour
     public void GotoScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-
+    }
+    public void mission_complete()
+    {
+        if(PlayerPrefs.GetInt(SceneManager.GetActiveScene().name)==1)
+        {
+            Debug.Log("mission complete");
+            SceneManager.LoadScene("Map_Choice", LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.Log("plz complete");
+        }
     }
 }
