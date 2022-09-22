bb8c9317a91f227cc36eb67d11d0a00f7a377dab
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2022-09-22 21:32:59 +0900

    make jelly

---
+++ AR_tutorial/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int[] jellyGoldList;
+    public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level-1];
+    }
+}
