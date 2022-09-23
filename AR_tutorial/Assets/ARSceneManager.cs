7572c5faf3181c3d4c41e13e9be00f1d898d02d7
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2022-09-24 06:12:05 +0900

    make demo

--- AR_tutorial/Assets/ARSceneManager.cs
+++ AR_tutorial/Assets/ARSceneManager.cs
@@ -10,9 +10,11 @@ public class ARSceneManager : MonoBehaviour
     public GameObject quest_text_clear;
     public GameObject quest_text_noclear;
     public GameObject Clear_btn;
-    public GameObject IT_1_check;
+    public GameObject Ground_check;
     public GameObject Main_Gate_check;
     public GameObject Lake_check;
+    public GameObject North_Gate_check;
+    public GameObject Library_check;
     public void GotoMain()
     {
         SceneManager.LoadScene("Main", LoadSceneMode.Single);
@@ -48,9 +50,9 @@ public class ARSceneManager : MonoBehaviour
 
     public void Reset_btn()
     {
-        PlayerPrefs.SetInt("IT1_map", 0);
+        PlayerPrefs.SetInt("Ground_map", 0);
        
-        IT_1_check.SetActive(false);
+        Library_check.SetActive(true);
         
         PlayerPrefs.SetInt("Lake_map", 0);
         Lake_check.SetActive(false);
--- AR_tutorial/Assets/stage_check.cs
+++ AR_tutorial/Assets/stage_check.cs
@@ -5,28 +5,38 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class stage_check : MonoBehaviour
 {
-    public GameObject IT_1_check;
+    public GameObject North_Gate_check;
     public GameObject Main_Gate_check;
     public GameObject Lake_check;
+    public GameObject Library_check;
+    public GameObject Ground_check;
     // Start is called before the first frame update
-   
+
     // Update is called once per frame
     void Update()
     {
-        if (PlayerPrefs.GetInt("IT1_map") == 1)
+        if (PlayerPrefs.GetInt("North_map") == 1)
         { 
-            IT_1_check.SetActive(true);
+            North_Gate_check.SetActive(false);
         }
         
         if (PlayerPrefs.GetInt("Main_Gate_map") == 1)
         {
-           Main_Gate_check.SetActive(true);
+           Main_Gate_check.SetActive(false);
         }
        
         if (PlayerPrefs.GetInt("Lake_map") == 1)
         {
-            Lake_check.SetActive(true);
+            Lake_check.SetActive(false);
         }
-        
+        if (PlayerPrefs.GetInt("Library_map") == 1)
+        {
+            Library_check.SetActive(false);
+        }
+        if (PlayerPrefs.GetInt("Ground_map") == 1)
+        {
+            Ground_check.SetActive(false);
+        }
+
     }
 }
