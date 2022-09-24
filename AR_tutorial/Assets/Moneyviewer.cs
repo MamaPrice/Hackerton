7bbf2a439778d32289b5fbf2c6c0a7ee591654df
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2022-09-25 02:54:33 +0900

    add money

--- AR_tutorial/Assets/Moneyviewer.cs
+++ AR_tutorial/Assets/Moneyviewer.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Moneyviewer : MonoBehaviour
 {
-    public TextMeshProUGUI MoneyScore;
+    public Text MoneyScore;
     // Start is called before the first frame update
     
 
